1c5a04a2e66c4afea3b5fddfe23b0e64527fcd5a
Daniel <daniel@daniellaing.com>
2024-03-13 15:56:23 +0000

    Formatting: Added conform plugin

--- config/formatting.nix
+++ config/formatting.nix
@@ -1,3 +1,23 @@
+{ pkgs, ... }:
+let
+  lua = pkgs.stylua;
+  all = pkgs.codespell;
+in
 {
-  plugins.conform-nvim = { };
+  plugins.conform-nvim = {
+    enable = true;
+    formattersByFt = {
+      lua = [ "${lua.pname}" ];
+      "*" = [ "${all.pname}" ];
+    };
+    formatOnSave = {
+      lspFallback = true;
+      timeoutMs = 500;
+    };
+  };
+
+  extraPackages = [
+    lua
+    all
+  ];
 }
