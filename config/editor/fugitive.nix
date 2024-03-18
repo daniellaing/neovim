1e8f9bed974b4a7f704ce02a15c26d2414216dfb
Daniel <daniel@daniellaing.com>
2024-03-18 12:18:21 +0000

    Editor: Added fugitive keymap

--- config/editor/fugitive.nix
+++ config/editor/fugitive.nix
@@ -2,4 +2,12 @@
   plugins.fugitive = {
     enable = true;
   };
+
+  keymaps = [
+    {
+      key = "<Leader>g";
+      action = "<CMD>G<Enter>";
+      options.desc = "Open git dashboard";
+    }
+  ];
 }
