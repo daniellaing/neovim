64d35ddc573efd91e2dfcd738141ed624625e96e
Daniel <daniel@daniellaing.com>
2024-03-22 11:46:18 +0000

    Python: Temporarily disable `pylyzer`, wait for mtshiba/pylyzer#78 to resolve

--- config/coding/langs/python.nix
+++ config/coding/langs/python.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
-  plugins.lsp.servers.pylyzer.enable = true;
+  # FIXME: Uncomment when mtshiba/pylyzer#78 resolves
+  # plugins.lsp.servers.pylyzer.enable = true;
 
   plugins.conform-nvim.formattersByFt.python = ["black"];
   extraPackages = [pkgs.black];
