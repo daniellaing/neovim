84bc456d44995176c7df6d0224d3db16401f5a59
Daniel <daniel@daniellaing.com>
2025-01-16 09:49:50 +0000

    Java: Set tab size to 2, see style guides.

--- config/coding/langs/java.nix
+++ config/coding/langs/java.nix
@@ -1,8 +1,27 @@
-{pkgs, ...}: {
+{
+  helpers,
+  pkgs,
+  ...
+}: {
   plugins = {
     lsp.servers.jdtls.enable = true;
     conform-nvim.settings.formatters_by_ft.java = ["google-java-format"];
     lint.lintersByFt.java = ["checkstyle"];
   };
   extraPackages = [pkgs.google-java-format pkgs.checkstyle];
+
+  autoCmd = [
+    {
+      event = "FileType";
+      desc = "Set tab size to 2 for java files";
+      group = "dlaing";
+      pattern = "java";
+      callback = helpers.mkRaw ''
+        function()
+            vim.opt_local.tabstop = 2
+            vim.opt_local.softtabstop = 2
+        end
+      '';
+    }
+  ];
 }
