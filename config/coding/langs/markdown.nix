26e375bff33c387de8e3e74d6cfd1d3e571ab507
Daniel <daniel@daniellaing.com>
2025-01-20 10:49:08 +0000

    Markdown: Added preview plugin

--- config/coding/langs/markdown.nix
+++ config/coding/langs/markdown.nix
@@ -2,6 +2,9 @@
   plugins = {
     lsp.servers.marksman.enable = true;
     conform-nvim.settings.formatters_by_ft.markdown = ["cbfmt" "mdformat"];
+    markdown-preview = {
+      enable = true;
+    };
   };
   extraPackages = with pkgs; [cbfmt mdformat];
 }
