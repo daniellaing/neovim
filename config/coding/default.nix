15a629fe246ef4a7e4653b232797b80d3a380318
Daniel <daniel@daniellaing.com>
2025-02-21 13:37:44 +0000

    Conform: Improved configuration

--- config/coding/default.nix
+++ config/coding/default.nix
@@ -12,8 +12,44 @@
   # ---   Formatting   ---
   plugins.conform-nvim = {
     enable = true;
-    settings.format_after_save = {
-      lspFallback = false;
+    luaConfig.pre = ''local slow_format_filetypes = {}'';
+    settings = {
+      format_on_save =
+        # Lua
+        ''
+          function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
+            end
+
+            if slow_format_filetypes[vim.bo[bufnr].filetype] then
+              return
+            end
+
+            local function on_format(err)
+              if err and err:match("timeout$") then
+                slow_format_filetypes[vim.bo[bufnr].filetype] = true
+              end
+            end
+
+            return { timeout_ms = 200, lsp_fallback = false }, on_format
+           end
+        '';
+      format_after_save =
+        # Lua
+        ''
+          function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
+            end
+
+            if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+              return
+            end
+
+            return { lsp_fallback = false }
+          end
+        '';
     };
   };
 
