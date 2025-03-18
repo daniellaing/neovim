2c48cbbfa096a88516bd7979d198c0e5fa5f487d
Daniel <daniel@daniellaing.com>
2025-03-18 21:02:47 +0000

    Improved notifications

--- config/ui.nix
+++ config/ui.nix
@@ -26,6 +26,16 @@
   # ---   Notify   ---
   plugins.notify = {
     enable = true;
+    settings = {
+      background_colour = "#000000";
+      on_open = helpers.mkRaw ''
+        function(win)
+          local buf = vim.api.nvim_win_get_buf(win)
+          vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+        end
+      '';
+    };
+    luaConfig.post = ''vim.notify = require("notify")'';
   };
 
   keymaps = [
