9655af017d6fbbf3fd36c55d1a55e41cbd0b48ef
Daniel <daniel@daniellaing.com>
2025-03-18 22:09:33 +0000

    Treesitter text objects

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -10,13 +10,34 @@
       };
     };
 
-    # Additional treesitter based things
+    # ---   Additional treesitter based things   ---
     treesitter-context.enable = true;
+
     treesitter-refactor = {
       # Possible coincidence with lsp saga
       enable = true;
       highlightDefinitions.enable = true;
     };
-    treesitter-textobjects.enable = true; # Pretty sure this does nothing without keymaps definied
+
+    treesitter-textobjects = {
+      enable = true;
+      select = {
+        enable = true;
+        lookahead = true;
+        keymaps = {
+          "aa" = "@parameter.outer";
+          "ia" = "@parameter.inner";
+          "af" = "@function.outer";
+          "if" = "@function.inner";
+          "ac" = "@class.outer";
+          "ic" = "@class.inner";
+          "ii" = "@conditional.inner";
+          "ai" = "@conditional.outer";
+          "il" = "@loop.inner";
+          "al" = "@loop.outer";
+          "at" = "@comment.outer";
+        };
+      };
+    };
   };
 }
