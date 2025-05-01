b76bd6bfd95c96cec2146c695e6f094a1a2cb8ec
Daniel <daniel@daniellaing.com>
2025-05-01 13:23:31 +0100

    DAP: Do not close ui so hastily

--- config/coding/dap.nix
+++ config/coding/dap.nix
@@ -5,8 +5,8 @@
     luaConfig.post = ''
       require("dap").listeners.before.attach.dapui_config = function() require("dapui").open() end
       require("dap").listeners.before.launch.dapui_config = function() require("dapui").open() end
-      require("dap").listeners.before.event_terminated.dapui_config = function() require("dapui").close() end
-      require("dap").listeners.before.event_exited.dapui_config = function() require("dapui").close() end
+      -- require("dap").listeners.before.event_terminated.dapui_config = function() require("dapui").close() end
+      -- require("dap").listeners.before.event_exited.dapui_config = function() require("dapui").close() end
     '';
   };
 
