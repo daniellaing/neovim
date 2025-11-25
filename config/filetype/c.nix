23d3eda9cba3947a819fd2f984677e4bb829cacb
Daniel Laing <daniel@daniellaing.com>
2025-11-25 14:02:12 +0000

    lsp/c: Init

---
+++ config/filetype/c.nix
@@ -0,0 +1,33 @@
+{lib, ...}: {
+  lsp.servers.clangd = {
+    enable = true;
+    package = null;
+    config.cmd = [
+      "clangd"
+      "--background-index"
+      "--clang-tidy"
+      "--completion-style=bundled"
+      "--header-insertion=iwyu"
+      "--header-insertion-decorators"
+    ];
+  };
+
+  plugins = {
+    conform-nvim.settings.formatters_by_ft.c = ["clang-format"];
+  };
+
+  autoCmd = [
+    {
+      event = "FileType";
+      desc = "Set C specifict configuration";
+      group = "dlaing";
+      pattern = "c";
+      callback = lib.nixvim.mkRaw ''
+        function()
+            vim.opt_local.tabstop = 2
+            vim.opt_local.softtabstop = 2
+        end
+      '';
+    }
+  ];
+}
