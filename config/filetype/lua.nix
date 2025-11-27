79767e9a2c8f83963cba29aae64919a6073a9f1d
Daniel Laing <daniel@daniellaing.com>
2025-11-27 16:58:42 +0000

    lua: Add keymap to execute lua code

--- config/filetype/lua.nix
+++ config/filetype/lua.nix
@@ -1,8 +1,29 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   lsp.servers.lua_ls.enable = true;
   plugins = {
     conform-nvim.settings.formatters_by_ft.lua = ["stylua"];
     lint.lintersByFt.lua = ["luacheck"];
   };
   extraPackages = [pkgs.stylua pkgs.luajitPackages.luacheck];
+
+  autoCmd = [
+    {
+      event = "FileType";
+      desc = "Set lua specific configuration";
+      group = "dlaing";
+      pattern = "lua";
+      callback = lib.nixvim.mkRaw ''
+        function()
+          vim.keymap.set('n', '<Leader>x', function()
+            vim.cmd("write")
+            vim.cmd('vsplit | term nvim -l %')
+          end, { buffer = true })
+        end
+      '';
+    }
+  ];
 }
