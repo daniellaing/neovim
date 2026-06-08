15aa168a87d5c59cd03841aac40101e2dba33c9b
Daniel Laing <daniel@daniellaing.com>
2026-06-08 14:28:19 +0100

    markdown: Tweaks to markdown configuration Remove `cbfmt` Add a couple of snippets

--- config/filetype/markdown/default.nix
+++ config/filetype/markdown/default.nix
@@ -3,12 +3,12 @@
 in {
   lsp.servers.marksman.enable = true;
   plugins = {
-    conform-nvim.settings.formatters_by_ft.markdown = ["cbfmt" "mdformat"];
+    conform-nvim.settings.formatters_by_ft.markdown = ["mdformat"];
     markdown-preview = {
       enable = true;
     };
   };
-  extraPackages = with pkgs; [cbfmt mdf];
+  extraPackages = [mdf];
 
   extraFiles."luasnippets/markdown.lua".source = ./snippets.lua;
 }
